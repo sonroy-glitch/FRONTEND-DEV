4127906e3809ca076b35f8042078fa50a10a7a06
Sr20044390 <rsounak55@gmail.com>
2024-01-06 00:38:05 +0530

    6.1.24

--- JS/COHORT/cohort-test.js
+++ JS/COHORT/cohort-test.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.post("/", (req, res) => {
-  var a = req.headers.authentication;
+  var a = req.headers.authentication; //gets authentication header data
   console.log(a);
   res.json(a);
 });
